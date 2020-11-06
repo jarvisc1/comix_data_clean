ba584a346ac61684e1b6d23a4cd81652a36c55b5
C Jarvis <Christopher1.jarvis@gmail.com>
2020-11-06 14:16:18 +0000

    Add filepaths and recoed of packages used

---
+++ r/00_install_packages.R
@@ -0,0 +1,9 @@
+## Packages
+
+## Here we record all the packages used in the analyses
+
+pkgs <- c("data.table",
+  "qs",
+  )
+
+install.packages(pkgs)
---
+++ r/00_setup_filepaths.r
@@ -0,0 +1,31 @@
+## Set use files paths. 
+
+## If you are a new user of the data you need to download LSHTM filesharing and get
+## access to the LSHTM Comix data folders
+
+
+## Filepaths for origin of spss files
+## CIJ
+if(Sys.info()["nodename"] == "DESKTOP-OKJFGKO"){
+  #dir_data_spss <- "data/spss"
+  dir_data_spss <- '~/../Filr/Net Folders/EPH Shared/Comix_survey/spss_files/spss/'
+  ## Filepaths for temp processing files
+  dir_data_process <- "data/processing"
+}
+
+## AG
+if(Sys.info()["nodename"] == "Amys-MacBook-Pro.local"){
+  #dir_data_spss <- "data/spss"
+  dir_data_spss <- "~/../amygimma/Filr/Net Folders/EPH Shared/Comix_survey/spss_files/spss/"
+  ## Filepaths for temp processing files
+  dir_data_process <- "data/processing"
+}
+
+
+## KLM
+if(Sys.info()["nodename"] == "DESKTOP-R36S69R"){
+  #dir_data_spss <- "data/spss"
+  dir_data_spss <- 'C:/Users/kw/Filr/Net Folders/EPH Shared/Comix_survey/spss_files/spss/'
+  ## Filepaths for temp processing files
+  dir_data_process <- "data/processing"
+}
