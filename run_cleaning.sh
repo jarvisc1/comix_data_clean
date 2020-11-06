b96a15a4b8e416c9ec1231b5e1f46aec288dff5f
C Jarvis <Christopher1.jarvis@gmail.com>
2020-11-06 15:25:03 +0000

    Add dm02 to shell script

--- run_cleaning.sh
+++ run_cleaning.sh
@@ -4,3 +4,6 @@
 echo "Convert from SPSS to QS files"
 Rscript.exe ".\r\dm01_resave_spss_as_qs.R"
 
+echo "Check and add country panel and wave variables"
+Rscript.exe ".\r\dm02_add_cnty_panel_wave_all.R"
+
