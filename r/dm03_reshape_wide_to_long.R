c6b054c98114d218bad88ba28bdbe95fa21893a0
C Jarvis <Christopher1.jarvis@gmail.com>
2020-11-06 15:30:31 +0000

    Add reshaping function for survey data and process from wide to long

---
+++ r/dm03_reshape_wide_to_long.R
@@ -0,0 +1,45 @@
+## Name: dm03_reshape_wide_to_long.R
+## Description: Reshape the survey data from wide to long and deal with the 
+##              various type of scale and loop variables that are in the survey
+## Input file:  cnty_wkN_yyyymmdd_pN_wvN_2.qs
+## Functions:   survey_to_datatable
+## Output file: cnty_wkN_yyyymmdd_pN_wvN_3.qs
+
+
+# Packages ----------------------------------------------------------------
+library(data.table)
+
+# Source user written scripts ---------------------------------------------
+source('r/00_setup_filepaths.r')
+source('r/functions/survey_to_datatable.R')
+
+# Countries ---------------------------------------------------------------
+country_codes <- c("UK", "NL", "BE", "NO")
+
+for(country in country_codes){
+  print(paste0("Start: ", country))
+  
+  # Setup input and output data and filepaths -------------------------------
+  filenames <- readxl::read_excel('data/spss_files.xlsx', sheet = country)
+  filenames <- filenames[!is.na(filenames$spss_name),]
+  r_names <- filenames$r_name
+  
+  for(r_name in r_names){
+    input_name <-  paste0(r_name, "_2.qs")
+    output_name <- paste0(r_name, "_3.qs")
+    input_data <-  file.path(dir_data_process, input_name)
+    output_data <- file.path(dir_data_process, output_name)
+  
+    dt <- qs::qread(input_data)
+    print(paste0("Opened: ", input_name)) 
+    
+    ## User written function
+    dt <- survey_to_datatable(dt)
+    
+    ## Save _3 data
+    qs::qsave(dt, file = output_data)
+    print(paste0('Saved:' , output_name))
+  }
+}
+
+  
