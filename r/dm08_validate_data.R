39541e9278509bd5b4db3abdd64ad9af8fcf5a2a
C Jarvis <Christopher1.jarvis@gmail.com>
2020-11-08 10:22:28 +0000

    Add validator package

--- r/dm08_validate_data.R
+++ r/dm08_validate_data.R
@@ -1,5 +1,5 @@
-## Name: dm_create_new_vars.R
-## Description: Add new variables to the dataset.
+## Name: dm_validate_data.R
+## Description: Peform data checks.
 ## Input file: combined_8.qs
 ## Functions:
 ## Output file: combined_9.qs
@@ -8,6 +8,7 @@
 # Packages ----------------------------------------------------------------
 library(data.table)
 library(validate)
+library(errorlocate)
 
 
 # Source user written scripts ---------------------------------------------
@@ -25,8 +26,24 @@ print(paste0("Opened: ", input_name))
 
 # Validation rules --------------------------------------------------
 
+## Two approaches check_that for short checks
 
+checks <- check_that(dt, 
+          age_pos =  part_age > 0,
+          len_id = panel %in% LETTERS[1:6],
+          fd = date <= Sys.Date())
+summary(checks)
 
+## Validator for running a lot of rules
+# Then the rules and confronting are done in different steps. 
+v <- validator(
+          age_pos =  part_age > 0,
+          len_id = panel %in% LETTERS[1:6],
+          fd = date <= Sys.Date()
+          )
+
+cf <- confront(dt, v)
+summary(cf)
 
 # Save data ---------------------------------------------------------------
 qs::qsave(dt, file = output_data)
