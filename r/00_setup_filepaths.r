b2fb02a9bd36900b7fb0fc7a898af89f0d3f96b8
C Jarvis <Christopher1.jarvis@gmail.com>
2020-11-14 05:16:00 +0000

    Change archive filepath for cleaning

--- r/00_setup_filepaths.r
+++ r/00_setup_filepaths.r
@@ -17,7 +17,7 @@ if(Sys.info()["nodename"] == "DESKTOP-OKJFGKO"){
   parent_path <- '~/../Filr/Net Folders/EPH Shared/Comix_survey/new_do_not_remove'
   dir_data_spss <- file.path(parent_path, 'data/spss')
   dir_data_clean <- file.path(parent_path, 'data/clean')
-  dir_data_archive <- file.path(parent_path, 'data/archive')
+  dir_data_archive <- file.path(parent_path, 'data/clean/archive')
   ## Filepaths for temp processing files
   dir_data_process <- "data/processing"
 }
@@ -28,7 +28,7 @@ if(Sys.info()["nodename"] == "Amys-MacBook-Pro.local"){
   parent_path <- '~/../amygimma/Filr/Net Folders/EPH Shared/Comix_survey/new_do_not_remove'
   dir_data_spss <- file.path(parent_path, 'data/spss')
   dir_data_clean <- file.path(parent_path, 'data/clean')
-  dir_data_archive <- file.path(parent_path, 'data/archive')
+  dir_data_archive <- file.path(parent_path, 'data/clean/archive')
   ## Filepaths for temp processing files
   dir_data_process <- "data/processing"
 }
@@ -40,7 +40,7 @@ if(Sys.info()["nodename"] == "DESKTOP-R36S69R"){
   parent_path <- 'C:/Users/kw/Filr/Filr/Net Folders/EPH Shared/Comix_survey/new_do_not_remove'
   dir_data_spss <- file.path(parent_path, 'data/spss')
   dir_data_clean <- file.path(parent_path, 'data/clean')
-  dir_data_archive <- file.path(parent_path, 'data/archive')
+  dir_data_archive <- file.path(parent_path, 'data/clean/archive')
   ## Filepaths for temp processing files
   dir_data_process <- "data/processing"
 }
