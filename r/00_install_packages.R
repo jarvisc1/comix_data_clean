ba6ae357f616e37a7a1d626591d8d3f45b7e2f81
amyg225 <amyg225@gmail.com>
2020-12-18 00:23:25 +0000

    small changes to run the shell script on mac

--- r/00_install_packages.R
+++ r/00_install_packages.R
@@ -2,11 +2,13 @@
 
 ## Here we record all the packages used in the analyses
 
-pkgs <- c("data.table",
+pkgs <- c(
+  "data.table",
   "qs",
   "stringr",
   "lubridate",
   "readxl"
   )
 
-install.packages(setdiff(pkgs, rownames(installed.packages())))
+install.packages(setdiff(pkgs, rownames(installed.packages())), 
+                 repos="https://cran.ma.imperial.ac.uk/")
--- r/dm01_resave_spss_as_qs.R
+++ r/dm01_resave_spss_as_qs.R
@@ -25,13 +25,12 @@ for(country in country_codes){
   
   spss_names <- paste0(filenames$spss_name, ".sav")
   r_names <- paste0(filenames$r_name, "_1.qs")
-  
   for(i in 1:length(spss_names)){
   # for latest data.  
   #for(i in length(spss_names)){
     print(paste0("Opened: ",spss_names[i]))
     ## User written function: read sspss file save as qs
-    save_spss_qs(spss_names[i], r_names[i], country)
+    save_spss_qs(spss_names[i], r_names[i], tolower(country))
     print(paste0("Saved: ", r_names[i]))
   }  
 }
