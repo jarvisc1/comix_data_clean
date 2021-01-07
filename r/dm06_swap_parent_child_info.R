dbdd6c6f1ef326052c9c48a1ca8dc3f72273d21d
amyg225 <amyg225@gmail.com>
2021-01-07 01:17:00 +0000

    works

--- r/dm06_swap_parent_child_info.R
+++ r/dm06_swap_parent_child_info.R
@@ -57,9 +57,8 @@ dt[, child_id := first(child_id), by = .(country, panel, wave, part_id)]
 dt[sample_type == "child" & (child_id == row_id),
    parent_child := "child"
    ]
-dt[sample_type == "child" & row_id == 0,
-   row_id := 999
-]
+dt[sample_type == "child" & row_id %in% c(0,999), mixed_data := T]
+dt[sample_type == "child" & row_id == 0, row_id := 999]
 
 dt[sample_type == "child" & row_id == 999,
    parent_child := "parent"
@@ -76,14 +75,33 @@ dt[sample_type == "child",
 
 
 ## Fill in parent (part_id 999) household member data from participant data
-part_cols <- grep("part_[^id]", names(dt), value = T)
-part_cols <- grep("hhm", part_cols, value = T, invert = T)
+parent_cols <- 
+   c("part_age",  "part_antibody_test", "part_covid_test_past", 
+     "part_education", "part_educationplace_status", "part_employstatus", 
+     "part_ethnicity", "part_ethnicity2", "part_furloughed", "part_gender", 
+     "part_gender_nb", "part_handsanit3h", "part_handwash3h", "part_high_risk_v2", 
+     "part_med_risk_v2", "part_no_contacts", "part_occupation", "part_occupation_prev", 
+     "part_pregnant", "part_reported_all_contacts", "part_social_group", 
+     "part_social_group1", "part_social_group2", "part_ukitv", "part_workplace_status"
+   )
 
-table(dt[parent_child == "parent"]$hhm_gender_nb, useNA = "always")
-for(part_col in part_cols) {
-   hhm_col <- gsub("part_", "hhm_", part_col)
-   dt[parent_child == "parent", (hhm_col) := get(part_col)]
+
+table(dt[parent_child == "parent"]$part_gender_nb, 
+      dt[parent_child == "parent"]$panel, useNA = "always")
+t <- Sys.time()
+for(parent_col in parent_cols) {
+   # parent_col <- "part_gender_nb"
+   hhm_col <- gsub("part_", "hhm_", parent_col)
+   dt[row_id  %in% c(999), 
+      (hhm_col) := first(get(parent_col)), 
+      by = .(part_id, panel, wave, country)]
+   # dt[row_id == 999 & !panel %in% c("C", "D"), 
+   #    (hhm_col) := get(parent_col)]
 }
+table(dt[row_id == 999]$hhm_gender_nb, useNA = "always")
+table(dt[row_id == 999]$hhm_gender_nb,
+      dt[ row_id == 999]$panel, useNA = "always")
+
 
 ## Fill in child (part_id 0) partdata from hhm data
 hhm_cols <- c("hhm_gender", "hhm_age_group")
@@ -93,7 +111,42 @@ for(hhm_col in hhm_cols) {
    part_col <- gsub("hhm_", "part_", hhm_col)
    dt[parent_child == "child", (part_col) := get(hhm_col)]
 }
-table(dt[parent_child == "child"]$part_age_group)
+
+## Fill in child (part_id 0) mixed_data row
+# child_part_cols <- grep("transport|class_size|attend|social_group|region|area",
+# names(dt), value = T)
+# child_part_cols <- grep("visit|nl|hhm|cnt|contact|symp",
+#                         child_part_cols, value = T, invert = T)
+child_part_cols  <- 
+   c("area_1_name","area_2_name", "area_3_name",  "area_4_name", 
+     "area_5_name", "area_pop_dens_1_label", "area_pop_dens_2_label", 
+     "area_rural_urban_label", "area_town_label", "area_town_pop_label", "area_tv_station", 
+     "part_attend_school_yesterday", "part_face_mask",
+     "part_public_transport_boat", "part_public_transport_bus", "part_public_transport_bus_hours", 
+     "part_public_transport_bus_mins", "part_public_transport_no", 
+     "part_public_transport_plane", "part_public_transport_plane_hours", 
+     "part_public_transport_plane_mins", "part_public_transport_taxi_uber", 
+     "part_public_transport_taxi_uber_hours", "part_public_transport_taxi_uber_mins", 
+     "part_public_transport_train", "part_public_transport_train_hours", 
+     "part_public_transport_train_mins", "part_school_class_size", 
+     "part_social_group", "part_social_group1", "part_social_group2", 
+     "uk_region1", "uk_region2", "uk_region3", "uk_region3_label", 
+     "uk_stdregion", "ukregion1", "ukstdregion")
+
+table(dt[sample_type == "child" &  panel %in% c("C", "F") & row_id == 0]$part_public_transport_bus)
+t <- Sys.time()
+for(child_part_col in child_part_cols) {
+   # browser()
+   dt[sample_type == "child" & (row_id == 0 | mixed_data == T), 
+      (child_part_col) := first(get(child_part_col)), 
+      by = .(part_id, panel, wave, country)]
+   # dt[row_id == 999 & !panel %in% c("C", "D"), 
+   #    (hhm_col) := get(parent_col)]
+}
+
+
+
+
 
 # Add adult age group
 dt[parent_child == "parent", hhm_age_group := 
@@ -122,8 +175,14 @@ dt[parent_child == "parent", hhm_age_group :=
       ifelse(between(hhm_age, 70, 120), "70+", hhm_age_group)]
 
 
-# LAST STEP: 
-# Combine 2 parent dt rows into one for C and D (original IDS 0 and 999)
+## IMPORTANT
+## Remove now-reduntant mixed_data row
+dt <- dt[is.na(mixed_data)]
+
+Sys.time() - t
+
+table(dt[sample_type == "child" &  panel %in% c("C", "F") & row_id == 0]$part_public_transport_bus)
+table(dt[parent_child == "parent"]$hhm_age_group)
 
 # Save data ---------------------------------------------------------------
 qs::qsave(dt, file = output_data)
