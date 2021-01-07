c96eb37d60f9bbad88f0a4b5f84b97c53be3ab32
C Jarvis <Christopher1.jarvis@gmail.com>
2021-01-07 14:28:33 +0000

    Add version 3 for panels E and F up to child cleaning

--- run_version_2.sh
+++ run_version_2.sh
@@ -22,26 +22,26 @@ Rscript.exe ".\r\version_2\dm04_rename_vars_v2.R"
 echo "Combine all countries and waves together"
 Rscript.exe ".\r\version_2\dm05_combine_data_v2.R"
 
-echo "Add adult survey variable"
-Rscript.exe ".\r\version_2\dm06_swap_parent_child_info_v2.R"
-
-echo "Add the multiple contacts as multiple rows"
-Rscript.exe ".\r\version_2\dm07_allocate_multiple_contacts_v2.R"
-
-echo "Clean data needs for contact analyses"
-Rscript.exe ".\r\version_2\dm08_clean_contacts_v2.R"
-
-echo "Clean location data"
-Rscript.exe ".\r\version_2\dm09_clean_locations_v2.R"
-
-echo "Clean Households"
-Rscript.exe ".\r\version_2\dm20_clean_household_v2.R"
-
-echo "Clean Participants"
-Rscript.exe ".\r\version_2\dm22_clean_participant_v2.R"
-
-echo "Save data on LSHTM server"
-Rscript.exe ".\r\version_2\dm202_save_remote_v2.R"
+# echo "Add adult survey variable"
+# Rscript.exe ".\r\version_2\dm06_swap_parent_child_info_v2.R"
+# 
+# echo "Add the multiple contacts as multiple rows"
+# Rscript.exe ".\r\version_2\dm07_allocate_multiple_contacts_v2.R"
+# 
+# echo "Clean data needs for contact analyses"
+# Rscript.exe ".\r\version_2\dm08_clean_contacts_v2.R"
+# 
+# echo "Clean location data"
+# Rscript.exe ".\r\version_2\dm09_clean_locations_v2.R"
+# 
+# echo "Clean Households"
+# Rscript.exe ".\r\version_2\dm20_clean_household_v2.R"
+# 
+# echo "Clean Participants"
+# Rscript.exe ".\r\version_2\dm22_clean_participant_v2.R"
+# 
+# echo "Save data on LSHTM server"
+# Rscript.exe ".\r\version_2\dm202_save_remote_v2.R"
  
 
  
