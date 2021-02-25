3468650b7a3dc0edff43443f3fdfa4595589e7b0
amyg225 <amyg225@gmail.com>
2021-02-25 19:21:40 +0000

    update sh files for v4-v6 and spss_files.xlsx

--- run_version_5_mac.sh
+++ run_version_5_mac.sh
@@ -1,61 +1,52 @@
-
+country="BE"
 
 if [ -z $1 ]
   then
-  echo "Running local code run 'sh run_version_4_mac.sh BEdownload' to download data for BE NL for NL."
-elif [ $1 == 'BEdownload' ]
+  echo "Running local code run 'sh run_version_5_mac.sh download' to download data."
+elif [ $1 == 'download' ]
   then
-  country="BE"
   echo "Convert from SPSS to QS files"
-  Rscript "r/version_4/dm01_resave_spss_as_qs_v4.R" $country
+  Rscript "r/version_5/dm01_resave_spss_as_qs_v5.R" $country
   pwd
-elif [ $1 == 'NLdownload' ]
-  then
-  country="NL"
-  echo "Convert from SPSS to QS files"
-  Rscript "r/version_4/dm01_resave_spss_as_qs_v4.R" $country
-  pwd
-else
-  country=$1
 fi
 
 
 
 echo "Check and add country panel and wave variables"
-Rscript "r/version_4/dm02_data_standardise_v4.R" $country
+Rscript "r/version_5/dm02_data_standardise_v5.R" $country
 
 echo "Turn from wide data to long data.table"
-Rscript "r/version_4/dm03_create_datatable_v4.R" $country
+Rscript "r/version_5/dm03_create_datatable_v5.R" $country
 
 echo "Rename the variables"
-Rscript "r/version_4/dm04_rename_vars_v4.R" $country
+Rscript "r/version_5/dm04_rename_vars_v5.R" $country
 
 echo "Combine all countries and waves together"
-Rscript "r/version_4/dm05_combine_data_v4.R" $country
+Rscript "r/version_5/dm05_combine_data_v5.R" $country
 
 echo "Add adult survey variable"
-Rscript "r/version_4/dm06_swap_parent_child_info_v4.R"
+Rscript "r/version_5/dm06_swap_parent_child_info_v5.R"
 
 echo "Add the multiple contacts as multiple rows"
-Rscript "r/version_4/dm07_allocate_multiple_contacts_v4.R"
+Rscript "r/version_5/dm07_allocate_multiple_contacts_v5.R"
 
 echo "Clean data needs for contact analyses"
-Rscript "r/version_4/dm08_clean_contacts_v4.R"
+Rscript "r/version_5/dm08_clean_contacts_v5.R"
 
 echo "Clean location data"
-Rscript "r/version_4/dm09_clean_locations_v4.R"
+Rscript "r/version_5/dm09_clean_locations_v5.R"
 
 echo "Clean Households"
-Rscript "r/version_4/dm10_clean_household_v4.R"
+Rscript "r/version_5/dm10_clean_household_v5.R"
 
 echo "Clean Participants"
-Rscript "r/version_4/dm11_clean_participant_v4.R"
+Rscript "r/version_5/dm11_clean_participant_v5.R"
 
 echo "Save data locally"
-Rscript "r/version_4/dm100_save_locally_v4.R" $country
+Rscript "r/version_5/dm100_save_locally_v5.R" $country
 
 echo "Save data on LSHTM server"
-Rscript "r/version_4/dm101_save_remote_v4.R" $country
+Rscript "r/version_5/dm101_save_remote_v5.R" $country
 
 
 
--- run_version_6.sh
+++ run_version_6.sh
@@ -1,19 +1,13 @@
+country="NL"
 
 
 if [ -z $1 ]
   then
-  echo "Running local code run 'sh run_version_4_mac.sh BEdownload' to download data for BE NL for NL."
-elif [ $1 == 'BEdownload' ]
+  echo "Running local code run 'sh run_version_6_mac.sh download' to download data."
+elif [ $1 == 'download' ]
   then
-  country="BE"
   echo "Convert from SPSS to QS files"
-  Rscript "r/version_4/dm01_resave_spss_as_qs_v4.R" $country
-  pwd
-elif [ $1 == 'NLdownload' ]
-  then
-  country="NL"
-  echo "Convert from SPSS to QS files"
-  Rscript "r/version_4/dm01_resave_spss_as_qs_v4.R" $country
+  Rscript "r/version_6/dm01_resave_spss_as_qs_v6.R" $country
   pwd
 else
   country=$1
@@ -22,40 +16,40 @@ fi
 
 
 echo "Check and add country panel and wave variables"
-Rscript "r/version_4/dm02_data_standardise_v4.R" $country
+Rscript "r/version_6/dm02_data_standardise_v6.R" $country
 
 echo "Turn from wide data to long data.table"
-Rscript "r/version_4/dm03_create_datatable_v4.R" $country
+Rscript "r/version_6/dm03_create_datatable_v6.R" $country
 
 echo "Rename the variables"
-Rscript "r/version_4/dm04_rename_vars_v4.R" $country
+Rscript "r/version_6/dm04_rename_vars_v6.R" $country
 
 echo "Combine all countries and waves together"
-Rscript "r/version_4/dm05_combine_data_v4.R" $country
+Rscript "r/version_6/dm05_combine_data_v6.R" $country
 
 echo "Add adult survey variable"
-Rscript "r/version_4/dm06_swap_parent_child_info_v4.R"
+Rscript "r/version_6/dm06_swap_parent_child_info_v6.R"
 
 echo "Add the multiple contacts as multiple rows"
-Rscript "r/version_4/dm07_allocate_multiple_contacts_v4.R"
+Rscript "r/version_6/dm07_allocate_multiple_contacts_v6.R"
 
 echo "Clean data needs for contact analyses"
-Rscript "r/version_4/dm08_clean_contacts_v4.R"
+Rscript "r/version_6/dm08_clean_contacts_v6.R"
 
 echo "Clean location data"
-Rscript "r/version_4/dm09_clean_locations_v4.R"
+Rscript "r/version_6/dm09_clean_locations_v6.R"
 
 echo "Clean Households"
-Rscript "r/version_4/dm10_clean_household_v4.R"
+Rscript "r/version_6/dm10_clean_household_v6.R"
 
 echo "Clean Participants"
-Rscript "r/version_4/dm11_clean_participant_v4.R"
+Rscript "r/version_6/dm11_clean_participant_v6.R"
 
 echo "Save data locally"
-Rscript "r/version_4/dm100_save_locally_v4.R" $country
+Rscript "r/version_6/dm100_save_locally_v6.R" $country
 
 echo "Save data on LSHTM server"
-Rscript "r/version_4/dm101_save_remote_v4.R" $country
+Rscript "r/version_6/dm101_save_remote_v6.R" $country
 
 
 
