b700ba54d4943366d16f7f3490f4fb1353df3a23
C Jarvis <Christopher1.jarvis@gmail.com>
2021-03-03 11:35:27 +0000

    Update users

--- r/00_setup_filepaths.r
+++ r/00_setup_filepaths.r
@@ -58,20 +58,7 @@ if(Sys.info()["nodename"] == "DESKTOP-R36S69R"){
 ## PC
 if(Sys.info()["nodename"] == "pietro-XPS-15-9550"){
   #dir_data_spss <- "data/spss"
-  parent_path <- 'C:/Users/kw/Filr/Net Folders/EPH Shared/Comix_survey'
-  dir_data_spss <- file.path(parent_path, 'data/spss')
-  dir_data_clean <- file.path(parent_path, 'data/clean')
-  dir_data_archive <- file.path(parent_path, 'data/clean/archive')
-  dir_data_validate <- file.path(parent_path, 'data/validated')
-  dir_data_valid_archive <- file.path(parent_path, 'data/validated/archive')
-  ## Filepaths for temp processing files
-  dir_data_process <- "data/processing"
-}
-
-## LB
-if(Sys.info()["nodename"] == "RIVM-SF1-0196"){
-  #dir_data_spss <- "data/spss"
-  parent_path <- 'N:/2020 CoMixNL/comix_data_clean'
+  parent_path <- '/home/pietro/calcolo/COVID-19/comix_data_clean'
   dir_data_spss <- file.path(parent_path, 'data/spss')
   dir_data_clean <- file.path(parent_path, 'data/clean')
   dir_data_archive <- file.path(parent_path, 'data/clean/archive')
@@ -83,6 +70,20 @@ if(Sys.info()["nodename"] == "RIVM-SF1-0196"){
 
 ## JW
 if(Sys.info()["nodename"] == "LP10494-01"){
+  #dir_data_spss <- "data/spss"
+  parent_path <- 'C:/Users/lucp10494/Desktop/COVID-19 FOLDER/COVID-19 COMIX ANALYSIS FOLDER/COMIX IPSOS DATA CLEANING/comix_data_clean'
+  dir_data_spss <- file.path(parent_path, 'data/spss')
+  dir_data_clean <- file.path(parent_path, 'data/clean')
+  dir_data_archive <- file.path(parent_path, 'data/clean/archive')
+  dir_data_validate <- file.path(parent_path, 'data/validated')
+  dir_data_valid_archive <- file.path(parent_path, 'data/validated/archive')
+  ## Filepaths for temp processing files
+  dir_data_process <- "data/processing"
+}
+
+
+## LB
+if(Sys.info()["nodename"] == "RIVM-SF1-0196"){
   #dir_data_spss <- "data/spss"
   parent_path <- 'N:/2020 CoMixNL/comix_data_clean'
   dir_data_spss <- file.path(parent_path, 'data/spss')
