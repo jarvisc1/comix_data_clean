cf7cd7a1ef8a0b9231e3f700d3427a6c96623287
C Jarvis <Christopher1.jarvis@gmail.com>
2021-03-05 07:46:54 +0000

    Update cleaning script to run all cleaning. Move main R files to validation

--- run_cleaning.sh
+++ run_cleaning.sh
@@ -1,43 +1,28 @@
-#!/bin/bash
-# Data cleaning process
 
-# echo "Install packages if needed"
-# Rscript.exe ".\r\dm00_install_packages.R"
-# 
-# echo "Convert from SPSS to QS files"
-# Rscript.exe ".\r\dm01_resave_spss_as_qs.R"
-# 
-#  echo "Check and add country panel and wave variables $1"
-#  Rscript.exe ".\r\dm02_data_standardise.R"
-#  
-#  echo "Turn from wide data to long data.table"
-#  Rscript.exe ".\r\dm03_create_datatable.R"
+# Running all of the data cleaning from begining to end. 
 
-# echo "Rename the variables"
-# Rscript.exe ".\r\dm04_rename_vars.R"
-
-# echo "Combine all countries and waves together"
-# Rscript.exe ".\r\dm05_combine_data.R"
-# 
-# echo "Swap adult and child info for parent surveys"
-# Rscript.exe ".\r\dm06_swap_parent_child_info.R"
-# 
-# echo "Allocate mass contacts to contact rows"
-# Rscript.exe ".\r\dm07_allocate_multiple_contacts.R"
-# 
-# echo "Clean data needs for contact analyses"
-# Rscript.exe ".\r\dm08_clean_contacts.R"
-
-echo "Clean location data"
-Rscript.exe ".\r\dm09_clean_locations.R"
-
-echo "Clean Households"
-Rscript.exe ".\r\dm10_clean_household.R"
-
-echo "Clean Participants"
-Rscript.exe ".\r\dm11_clean_participant.R"
+# This probably unwise as it would make it hard to see errors.
 
 
-echo "Save data on LSHTM server"
-Rscript.exe ".\r\dm101_save_remote.R"
+# sh run_version_1.sh
+# sh run_version_2.sh
+# sh run_version_3.sh
+# sh run_version_3.sh 'latest'
+# sh run_version_4.sh 'G1'
+# sh run_version_4.sh 'G2'
+# sh run_version_5.sh
+# sh run_version_6.sh
+
+# sh run_version_1.sh 'download'
+# sh run_version_2.sh 'download'
+# sh run_version_3.sh 'download'
+# sh run_version_4.sh 'G1download'
+# sh run_version_4.sh 'G2download'
+# sh run_version_5.sh 'download'
+# sh run_version_6.sh 'download'
+
+Rscript.exe r/validation/dm00_combine.r
+Rscript.exe r/validation/dm01_update_values.r
+Rscript.exe r/validation/dm100_save_locally.r
+Rscript.exe r/validation/dm101_save_remote.r
  
